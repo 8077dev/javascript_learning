878bc311d5b6702292dab4f89e6cbd2f58e044b8
Devpriyam Agarwal <104711557+8077dev@users.noreply.github.com>
2024-12-28 06:06:06 +0000

    config done

---
+++ 01_basics/variable.js
@@ -0,0 +1,5 @@
+const accountID = 1234567
+let accountemail = "asdf@gmail.com"     //use only let in comparison to var//
+let accountpassword = 1234
+console.log([accountID, accountemail, accountpassword])
+console.table([accountID, accountemail, accountpassword])
